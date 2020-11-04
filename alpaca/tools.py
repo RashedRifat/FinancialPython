83564577cdf04e32b19d230a4684a28392f3fe55
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2020-11-04 01:50:59 -0500

    create supertrend + multi-timeframe

--- alpaca/tools.py
+++ alpaca/tools.py
@@ -1,19 +1,19 @@
 # File of Finacial Instruments To Be Used
 import pandas as pd
 
-'''
-Conidtions
-
-Long Impulse: [0, Decreasing], [1, Stable], [2, Increasing]
-Short Impulse: [0, Decreasing], [,1 Stable], [2, Increasing]
-Value Zone: [0, Above], [1, Below], [2, At]
-MACD: [0, Decreasing], [1, Stable], [2, Increasing]
-ADX Spread: [0, Increasing], [1, Stable], [2, Decreasing]
-
-'''
 
 def get_MA(stock, data, n, method=None):
-    '''Calculates the MACD for a stock'''
+    '''Calculates the moving average for a stock
+    
+    Args: 
+        stock (str): name of stock to be analyzed 
+        data (Pandas Dataframe): data frame object containing data for stock 
+        n (int): number of days for which to calculate MA for 
+        method (string): value by which to calculate MA for (ex: by closing price, daily low, etc)
+    
+    Returns:
+        ma: a pands Series with the moving average of the stock 
+    '''
 
     # Basic Error Checking
     if not isinstance(stock, str):
@@ -22,6 +22,8 @@ def get_MA(stock, data, n, method=None):
         raise TypeError(method, " must be a string!")
     if not isinstance(data, pd.DataFrame):
         raise TypeError("data must be a dataframe!")
+    if not isinstance(n, int):
+        raise TypeError("n must be a int")
 
     method = method.strip().lower() 
     stock = stock.strip().upper()
@@ -39,22 +41,52 @@ def get_MA(stock, data, n, method=None):
         raise AttributeError("data does not contain " + method + " data")
 
     prices = data[price_name]
-    to_return = prices.ewm(span=n, adjust=False).mean()
-    return to_return
+    ma = prices.ewm(span=n, adjust=False).mean()
+    return ma
 
 def get_MACD(stock, data, n=[12, 26], method=None):
+    '''Calculates the MACD for a stock
+    
+    Args: 
+        stock (str): name of stock to be analyzed 
+        data (Pandas Dataframe): data frame object containing data for stock 
+        n (list): list of days for which to calculate MACD for 
+        method (string): value by which to calculate MACD for (ex: by closing price, daily low, etc)
+    
+    Returns:
+        macd: a pands Series for the MACD of the stock 
+    '''
+
     if not isinstance(n, list) or len(n) != 2:
         raise TypeError("n must be a list of two days (spans)")
     
     macd_1 = get_MA(stock, data, n[0], method)
     macd_2 = get_MA(stock, data, n[1], method)
-    return macd_1 - macd_2
+    macd = macd_1 - macd_2
+    return macd
 
 def get_MACD_Signal(stock, data, n=[12, 26], method=None):
+    '''Calculates the MACD Signal line from a stock
+    
+    Args: 
+        stock (str): name of stock to be analyzed 
+        data (Pandas Dataframe): data frame object containing data for stock 
+        n (list): list of days for which to calculate MACD Signal Line for 
+        method (string): value by which to calculate MACD Singal Line for (ex: by closing price, daily low, etc)
+    
+    Returns:
+        macd: a pands Series for the MACD Signal line of the stock 
+    '''
+
     macd = get_MACD(stock, data, n, method)
-    return macd.ewm(span=n[0], adjust=False).mean()
+    macd_signal = macd.ewm(span=n[0], adjust=False).mean()
+    return macd_signal
 
 def get_SuperTrend(stock, data):
+    '''Calculates the Supertrend tool for a stock'''
+    
+    
+    
     return None 
 
 def get_ValueZone():
---
+++ alpaca/utils.py
@@ -0,0 +1,188 @@
+# Testing the Alpaca API
+import alpaca_trade_api as tradeapi
+import pandas as pd 
+import os 
+#import tqdm 
+import tools
+
+# Set up API and check to see that it is working 
+api = tradeapi.REST("hidden", "hidden", base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+try:
+    account = api.get_account()
+except Exception:
+    raise ConnectionError("Unable to get account for keys.")
+
+def check_for_data(stocks, start, end, timeframe):
+    '''Checks if stock data is already in the data directory by using the runs.txt file.
+    
+    Args:
+        stocks (list): list of stocks to check for
+        start (str): string representation of the start date 
+        end (str): string reprsentation of the end date
+        timeframe (str): timeframe for which the data should be checked
+
+    Returns:
+        returnStocks (list): list of stocks for which data has not been found
+    '''
+    # Implement further validation here - check if the stock name is within the list of directories 
+    # Check if the start and end dates match the dates within the file  
+    # Create a clear function that removes all of the files within the data file and use it for reset=True
+
+    if not isinstance(stocks, list):
+        raise TypeError("stocks must be a list!")
+    if not isinstance(start, str):
+        raise TypeError("start must be a string formatted YYYY-MM-DD!")
+    if not isinstance(end, str):
+        raise TypeError("end must be a string formatted YYYY-MM-DD!")
+
+    f = open("bin\\runs.txt", "a+")
+    f.seek(0)
+    lines = f.readlines()
+    returnStocks = list()
+    all_files = os.listdir("data")
+
+    # Check that stocks are in the directory
+    for stock in stocks:
+        search_text = stock.upper() + start + end + timeframe + "\n"
+        file_text = stock.upper() + "_" + timeframe + "_data.csv"
+        if search_text in lines and file_text in all_files:
+            continue
+        else:
+            if search_text in lines and file_text not in all_files:
+                print(stock + " data has been deleted, re-getting data.")
+            f.seek(2)
+            f.write(search_text)
+            returnStocks.append(stock.upper())
+    f.close()
+
+    return returnStocks
+
+
+def make_data_csv(stocks=["AAPL", "MSFT"], start="2020-10-01", end="2020-10-31", timeframe="day", reset=False):
+    '''Find data for the specificed stocks and create csv's in the data directory
+    
+    Args:
+        stocks (list): list of stocks to find data for 
+        start (str): string representation of the start date 
+        end (str): string reprsentation of the end date
+        timeframe (str): timeframe for getting data; can be minute, 1Min, 5Min, 15Min, day or 1D
+        reset (bool): if True, create new csv files regardless if data is already present 
+                      if False, create csv files for only missing stocks 
+
+    Returns:
+        None
+    '''
+
+    timeframe_list = ["minute", "1Min", "5Min", "15Min", "day", "1D"]
+    # Basic Error Checking
+    if not isinstance(stocks, list):
+        raise TypeError("stocks musbt be of type list with string values!")
+    if len(stocks) == 0:
+        raise ValueError("list of stocks musbt be greater than 0!")
+    if not isinstance(start, str):
+        raise TypeError("start must be a string formatted YYYY-MM-DD!")
+    if not isinstance(end, str):
+        raise TypeError("end must be a string formatted YYYY-MM-DD!")
+    if not isinstance(timeframe, str):
+        raise TypeError("timeframe must be of type str and be minute, 1Min, 5Min, 15Min, day or 1D")
+    if timeframe not in timeframe_list:
+        timeframe_str = ", ".join([elem for elem in timeframe_list])
+        raise ValueError("timeframe must be of: " + timeframe_str + ", not " + timeframe)
+
+    #Check for previous runs 
+    if reset:
+        all_files = os.listdir("data")
+        for nfile in all_files:
+            os.remove("data\\" + nfile)
+        new_runs = open("bin\\runs.txt", "w+")
+        for stock in stocks:
+            search_text = stock.upper() + start + end + timeframe + "\n"
+            new_runs.write(search_text)
+        new_runs.close()
+
+    else:
+        stocks = check_for_data(stocks, start, end, timeframe)
+        if len(stocks) == 0 and reset == False:
+            print("Data saved from previous runs, new csv('s) not needed.\nPlease check that old csv's have the correct time ranges.\n") 
+            return
+
+    # Get Data from API
+    NY = 'America/New_York'
+    start = pd.Timestamp(start, tz=NY).isoformat()
+    end = pd.Timestamp(end, tz=NY).isoformat()
+
+    try:
+        data = api.get_barset(stocks, timeframe, start=start, end=end).df
+    except Exception as e:
+        print("Alpaca API Error: Unable to get data for: ", stocks, "\n", e)
+        return 
+
+    # Save Data in CSV for later use
+    stop_index = len(data.columns)
+    if stop_index % 5 != 0:
+        raise TypeError("API did not return expected number of columns")
+    start_index = 0
+    end_index = 5
+
+    # Save Data    
+    while (start_index < stop_index):
+        stock_data = data[data.columns[start_index:end_index]]
+        stock_name = stock_data.columns[0][0].upper()
+        filename = "data\\" + stock_name + "_" + timeframe + "_data.csv"
+        stock_data.to_csv(filename)
+        start_index += 5
+        end_index += 5
+        print("Saved file: " + filename)
+    if not reset:
+        print("\nPlease ensure previous files in \\data has the correct time ranges.\nOtherwise, delete all files in \\data and run this program again.\n")
+
+def get_data(stocks, timeframe):
+    '''Get data from the data directory for the stocks given.
+    
+    Args:
+        stocks (list): list of stocks for which to get data for 
+        timeframe (str): timeframe for stock data; can be minute, 1Min, 5Min, 15Min, day or 1D
+
+    Returns:
+        stock_dict (dict): dictonary of stocks and the associated data 
+    '''
+
+    if not isinstance(stocks, list):
+        raise TypeError("get_data must have an input of type list!")
+    if not isinstance(timeframe, str):
+        raise TypeError("timeframe must be of type str and be minute, 1Min, 5Min, 15Min, day or 1D")
+    timeframe_list = ["minute", "1Min", "5Min", "15Min", "day", "1D"]
+    if timeframe not in timeframe_list:
+        timeframe_str = ", ".join([elem for elem in timeframe_list])
+        raise ValueError("timeframe must be of: " + timeframe_str + ", not " + timeframe)
+
+    stock_dict = dict()
+    for stock in stocks:
+        stock = stock.upper()
+        filename = "data\\" + stock + "_" + timeframe + "_data.csv"
+        
+        # Check if data exsists 
+        try:
+            stock_data = pd.read_csv(filename).dropna()
+        except:
+            print("Unable to find data for: ", stock, " skipping for now.")
+            continue
+
+        columns = ["date", stock + "_open", stock + "_high", stock + "_low", stock + "_close", stock + "_volume"]
+        stock_data.columns = columns
+        stock_dict[stock.upper()] = stock_data
+    
+    return stock_dict
+
+
+def run(stocks, start, end, reset=False, timeframe="day"):
+    '''Pesudo Main Class for running the File'''
+    make_data_csv(stocks, start, end, reset=reset, timeframe=timeframe)
+    stock_dict = get_data(stocks, timeframe)
+    AAPL = stock_dict["VYM"]
+    tools.get_MACD(stock="VYM", data=AAPL, n=[1,2], method="average")
+    
+
+# Test Methods
+run(stocks=["VYM", "GOOG", "T"], start="2020-10-01", end="2020-10-31", reset=False, timeframe="1D")
+#run(stocks=["AAPL", "MSFT", "TSLA"], start="2020-10-01", end="2020-10-31")
